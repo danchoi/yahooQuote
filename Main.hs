3b3875401195ee8cb1084ca586a86dba2b4ab217
Daniel Choi <dhchoi@gmail.com>
2014-03-04 19:28:32 -0500

    handle two types of errors

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main
 where
+import Control.Exception 
 import Text.CSV
 import Data.List (intercalate)
 import Data.Aeson
@@ -16,14 +17,24 @@ printJson :: String -> IO ()
 printJson sym = getJson sym >>= putStrLn . B.unpack
 
 getJson :: String -> IO B.ByteString
-getJson sym = do 
+getJson sym = (do 
       r <- fetch sym 
-      return $ encode $ either (const errorMsg) id $ getData r
+      return $ encode $ either (const $ errorMsg "No matching symbol") id $ getData r
+      ) `catch` (\e -> return . encode . errorMsg $ show (e :: SomeException))
+    where errorMsg e = Map.fromList [("Error", e)]
 
+{-
 
-errorMsg = Map.fromList [("Error", "No matching symbol")]
+  Contemplated errors are
 
--- feed fetch sym 
+    {"Error":"user error (openTCPConnection: host lookup failure for \"download.finance.yahoo.com\")"}
+
+  and
+
+    {"Error":"No matching symbol"}
+
+-}
+
 getData :: String -> Either String (Map.Map String String)
 getData sym = 
   let r = string2csv sym
@@ -43,8 +54,6 @@ string2csv s = case parseCSV "" s of
 formatResponse :: [[String]] -> [(String, String)]
 formatResponse (xs:_) = zip (map fst codes) xs 
 formatResponse [] = []
-
-testFetch = fetch "YHOO"
 
 fetch :: String -> IO String
 fetch sym = do
@@ -142,5 +151,7 @@ codes = [
        ]
 
 testcsv = "41.16,35.0994,37.9459,41.72,21.87,\"21.87 - 41.72\",\"N/A - N/A\",\"-\",39.70,600,39.70,17247400,39.63,300,39.63,12.587,\"+1.38 - +3.61%\",\"+1.38\",+4.5306,+1.6841,-2.09,+17.76,\"N/A - +3.61%\",\"+3.61%\",+1.38,-,39.79,38.68,\"N/A - N/A\",\"38.68 - 39.79\",\"N/A - N/A\",\"- - +3.61%\",\"N/A\",N/A,0.00,1.206B,1.58,0.37,1.80,1.26,\"N/A\",\"N/A\",   963,215,000,-,N/A,\"N/A - N/A\",\"- - -\",-,N/A,-,39.63,\"N/A - <b>39.63</b>\",\"Mar  4 - <b>39.63</b>\",\"3/4/2014\",535,222,\"4:00pm\",-,N/A,41.166B,\"cn\",\"Yahoo Inc.\",\"-\",38.74,\"N/A\",N/A,30.36,3.34,-5.01%,+12.91%,+4.44%,+81.21%,38.25,-,3.04,24.21,21.25,8.49,-,1.20,\"NasdaqNM\",\"YHOO\",\"&nbsp;===+=+&nbsp;\",-"
+
+testFetch = fetch "YHOO"
 
 
